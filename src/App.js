fefadcd3a7435dc5c08268c0a74bc5bc292fc062
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-16 15:12:13 +0600

    create array of object name product

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const product = [
+    {name: 'laptop', price: 53000},
+    {name: 'phone', price: 9300},
+    {name: 'watch', price: 3300},
+    {name: 'tablet', price: 20000},
+  ]
   return (
     <div className="App">
-      <Product name="Laptop" price="47000"></Product>
+      {/* <Product name="Laptop" price="47000"></Product>
       <Product name="phone" price="7500"></Product>
-      <Product name="Watch" price="3500"></Product>
+      <Product name="Watch" price="3500"></Product> */}
+      
     </div>
   );
 }
